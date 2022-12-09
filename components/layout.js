b54a56224fda2c4682c9ecedf30f9639c50daf3f
Misheel Munkh-Erdene <misheelmisheel651@gmail.com>
2022-12-09 09:06:33 +0800

    2 bolgoj uurchilluu

--- components/layout.js
+++ components/layout.js
@@ -64,7 +64,7 @@ export default function Layout({ children, home }) {
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
+          <Link href="/">← Back to home2</Link>
         </div>
       )}
     </div>
